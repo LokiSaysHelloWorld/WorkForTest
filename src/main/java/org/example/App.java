e6749d74b249b1eaab470e0f9ce3ea3de232ac8b
loki <lokysingh@gmail.com>
2024-06-03 11:28:45 +0530

    update to main method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String s = "yes";
+        System.out.printf("%s as caps %S",s ,s);
     }
 }
